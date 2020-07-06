dfcf18bf5408f90936a5fa91276d95e8f1520707
irof <irof.ocean@gmail.com>
2020-07-06 23:28:17 +0900

    fix

--- .github/actions/hello-javascript-action/index.js
+++ .github/actions/hello-javascript-action/index.js
@@ -1,6 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const twitter = require('twitter');
+const Twitter = require('twitter');
 
 try {
     console.log(`Hello ${github}!`);
